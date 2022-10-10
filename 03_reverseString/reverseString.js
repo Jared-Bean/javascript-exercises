7dea2becce8a2c2b831d6e2a6d284335ac600c65
Jared Bean <jaredbean0@gmail.com>
2022-10-09 21:59:44 -0400

    Complete reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(s) {
+    if (s == "") return "";
+    let result = ""
+    for (let i = s.length - 1; i >= 0; i--)
+    {
+        result+= s[i]; 
+    }
+    return result;
 
 };
 
