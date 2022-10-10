fe3b0b6f56f1bd38e0d2b64526189da323427ae4
Jared Bean <jaredbean0@gmail.com>
2022-10-09 22:24:51 -0400

    Completed sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof(a) != "number" || typeof(b) != "number") return "ERROR";
+    if (a < 0 || b < 0) return "ERROR";
+    result = 0;
+    if (b < a) 
+    {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+    for (let i = a; i <= b; i++)
+    {
+        result += i;
+    }
+    return result;
 
 };
 
